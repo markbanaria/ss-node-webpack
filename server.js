7f0a8c0c4d55268ad610f62ae0d1e6fbd559fc7e
markbanaria <markbanaria@gmail.com>
2015-06-14 01:13:46 +0800

    asdf

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var proxy = httpProxy.createProxyServer({
 }); 
 var app = express();
 var isProduction = process.env.NODE_ENV === 'production';
-var port = isProduction ? process.env.PORT : 3001;
+var port = isProduction ? process.env.PORT : 3000;
 var publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
